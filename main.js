24eb1ab4bf6f808940ac0bb637c1be5e8e5f70f9
Sher06777 <132756489+Sher06777@users.noreply.github.com>
2023-05-19 20:47:32 +0600

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,14 @@
 let justNumber = prompt('Введите число')
-if (justNumber % 2 == 0) {
-    alert('Число чётное');
-} else {
-    alert('Число нечётное'); 
+if (age === null){
+    alert('Вы нажали на отмену');
+}else if (age === ""){
+    alert('Вы ничего не ввели');
+}else if (age === '0'){
+    alert('Число равно нулю');
+}else if (age % 2 === 0){
+    alert('Чётное');
+}else if (age % 2 === 1){
+    alert('Нечётное');
+}else{
+    alert('Введите только числа');
 }
